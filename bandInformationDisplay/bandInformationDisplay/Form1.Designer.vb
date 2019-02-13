892a7a5bfdb1296796fca5d6969f644bd893c0da
Arnavpmr <31780071+Arnavpmr@users.noreply.github.com>
2019-02-13 09:49:04 -0500

    Add files via upload

---
+++ bandInformationDisplay/bandInformationDisplay/Form1.Designer.vb
@@ -0,0 +1,189 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.grpSelectBand = New System.Windows.Forms.GroupBox()
+        Me.radNirvana = New System.Windows.Forms.RadioButton()
+        Me.radOneDirection = New System.Windows.Forms.RadioButton()
+        Me.radFooFighters = New System.Windows.Forms.RadioButton()
+        Me.radRedHotChilliPeppers = New System.Windows.Forms.RadioButton()
+        Me.radBeatles = New System.Windows.Forms.RadioButton()
+        Me.lblBandMember1 = New System.Windows.Forms.Label()
+        Me.lblBandMember2 = New System.Windows.Forms.Label()
+        Me.lblBandMember3 = New System.Windows.Forms.Label()
+        Me.lblBandMember4 = New System.Windows.Forms.Label()
+        Me.lblBandMember5 = New System.Windows.Forms.Label()
+        Me.grpSelectBand.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'grpSelectBand
+        '
+        Me.grpSelectBand.Controls.Add(Me.radNirvana)
+        Me.grpSelectBand.Controls.Add(Me.radOneDirection)
+        Me.grpSelectBand.Controls.Add(Me.radFooFighters)
+        Me.grpSelectBand.Controls.Add(Me.radRedHotChilliPeppers)
+        Me.grpSelectBand.Controls.Add(Me.radBeatles)
+        Me.grpSelectBand.Location = New System.Drawing.Point(12, 12)
+        Me.grpSelectBand.Name = "grpSelectBand"
+        Me.grpSelectBand.Size = New System.Drawing.Size(200, 237)
+        Me.grpSelectBand.TabIndex = 0
+        Me.grpSelectBand.TabStop = False
+        Me.grpSelectBand.Text = "Select a Band"
+        '
+        'radNirvana
+        '
+        Me.radNirvana.AutoSize = True
+        Me.radNirvana.Location = New System.Drawing.Point(19, 201)
+        Me.radNirvana.Name = "radNirvana"
+        Me.radNirvana.Size = New System.Drawing.Size(62, 17)
+        Me.radNirvana.TabIndex = 4
+        Me.radNirvana.TabStop = True
+        Me.radNirvana.Text = "Nirvana"
+        Me.radNirvana.UseVisualStyleBackColor = True
+        '
+        'radOneDirection
+        '
+        Me.radOneDirection.AutoSize = True
+        Me.radOneDirection.Location = New System.Drawing.Point(19, 157)
+        Me.radOneDirection.Name = "radOneDirection"
+        Me.radOneDirection.Size = New System.Drawing.Size(90, 17)
+        Me.radOneDirection.TabIndex = 3
+        Me.radOneDirection.TabStop = True
+        Me.radOneDirection.Text = "One Direction"
+        Me.radOneDirection.UseVisualStyleBackColor = True
+        '
+        'radFooFighters
+        '
+        Me.radFooFighters.AutoSize = True
+        Me.radFooFighters.Location = New System.Drawing.Point(19, 112)
+        Me.radFooFighters.Name = "radFooFighters"
+        Me.radFooFighters.Size = New System.Drawing.Size(83, 17)
+        Me.radFooFighters.TabIndex = 2
+        Me.radFooFighters.TabStop = True
+        Me.radFooFighters.Text = "Foo Fighters"
+        Me.radFooFighters.UseVisualStyleBackColor = True
+        '
+        'radRedHotChilliPeppers
+        '
+        Me.radRedHotChilliPeppers.AutoSize = True
+        Me.radRedHotChilliPeppers.Location = New System.Drawing.Point(19, 73)
+        Me.radRedHotChilliPeppers.Name = "radRedHotChilliPeppers"
+        Me.radRedHotChilliPeppers.Size = New System.Drawing.Size(131, 17)
+        Me.radRedHotChilliPeppers.TabIndex = 1
+        Me.radRedHotChilliPeppers.TabStop = True
+        Me.radRedHotChilliPeppers.Text = "Red Hot Chilli Peppers"
+        Me.radRedHotChilliPeppers.UseVisualStyleBackColor = True
+        '
+        'radBeatles
+        '
+        Me.radBeatles.AutoSize = True
+        Me.radBeatles.Location = New System.Drawing.Point(19, 32)
+        Me.radBeatles.Name = "radBeatles"
+        Me.radBeatles.Size = New System.Drawing.Size(82, 17)
+        Me.radBeatles.TabIndex = 0
+        Me.radBeatles.TabStop = True
+        Me.radBeatles.Text = "The Beatles"
+        Me.radBeatles.UseVisualStyleBackColor = True
+        '
+        'lblBandMember1
+        '
+        Me.lblBandMember1.AutoSize = True
+        Me.lblBandMember1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBandMember1.Location = New System.Drawing.Point(293, 44)
+        Me.lblBandMember1.Name = "lblBandMember1"
+        Me.lblBandMember1.Size = New System.Drawing.Size(22, 13)
+        Me.lblBandMember1.TabIndex = 1
+        Me.lblBandMember1.Text = "-----"
+        '
+        'lblBandMember2
+        '
+        Me.lblBandMember2.AutoSize = True
+        Me.lblBandMember2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBandMember2.Location = New System.Drawing.Point(293, 85)
+        Me.lblBandMember2.Name = "lblBandMember2"
+        Me.lblBandMember2.Size = New System.Drawing.Size(22, 13)
+        Me.lblBandMember2.TabIndex = 2
+        Me.lblBandMember2.Text = "-----"
+        '
+        'lblBandMember3
+        '
+        Me.lblBandMember3.AutoSize = True
+        Me.lblBandMember3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBandMember3.Location = New System.Drawing.Point(293, 124)
+        Me.lblBandMember3.Name = "lblBandMember3"
+        Me.lblBandMember3.Size = New System.Drawing.Size(22, 13)
+        Me.lblBandMember3.TabIndex = 3
+        Me.lblBandMember3.Text = "-----"
+        '
+        'lblBandMember4
+        '
+        Me.lblBandMember4.AutoSize = True
+        Me.lblBandMember4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBandMember4.Location = New System.Drawing.Point(293, 169)
+        Me.lblBandMember4.Name = "lblBandMember4"
+        Me.lblBandMember4.Size = New System.Drawing.Size(22, 13)
+        Me.lblBandMember4.TabIndex = 4
+        Me.lblBandMember4.Text = "-----"
+        '
+        'lblBandMember5
+        '
+        Me.lblBandMember5.AutoSize = True
+        Me.lblBandMember5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBandMember5.Location = New System.Drawing.Point(293, 213)
+        Me.lblBandMember5.Name = "lblBandMember5"
+        Me.lblBandMember5.Size = New System.Drawing.Size(22, 13)
+        Me.lblBandMember5.TabIndex = 5
+        Me.lblBandMember5.Text = "-----"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(603, 261)
+        Me.Controls.Add(Me.lblBandMember5)
+        Me.Controls.Add(Me.lblBandMember4)
+        Me.Controls.Add(Me.lblBandMember3)
+        Me.Controls.Add(Me.lblBandMember2)
+        Me.Controls.Add(Me.lblBandMember1)
+        Me.Controls.Add(Me.grpSelectBand)
+        Me.Name = "Form1"
+        Me.Text = "Band Information"
+        Me.grpSelectBand.ResumeLayout(False)
+        Me.grpSelectBand.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents grpSelectBand As System.Windows.Forms.GroupBox
+    Friend WithEvents radNirvana As System.Windows.Forms.RadioButton
+    Friend WithEvents radOneDirection As System.Windows.Forms.RadioButton
+    Friend WithEvents radFooFighters As System.Windows.Forms.RadioButton
+    Friend WithEvents radRedHotChilliPeppers As System.Windows.Forms.RadioButton
+    Friend WithEvents radBeatles As System.Windows.Forms.RadioButton
+    Friend WithEvents lblBandMember1 As System.Windows.Forms.Label
+    Friend WithEvents lblBandMember2 As System.Windows.Forms.Label
+    Friend WithEvents lblBandMember3 As System.Windows.Forms.Label
+    Friend WithEvents lblBandMember4 As System.Windows.Forms.Label
+    Friend WithEvents lblBandMember5 As System.Windows.Forms.Label
+
+End Class
---
+++ bandInformationDisplay/bandInformationDisplay/Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+    'Arnav
+    '10/12
+    'BandInformationDisplay
+
+    Private Sub displayBeatlesInfo(ByVal sender As Object, ByVal e As System.EventArgs) Handles radBeatles.Click
+        Me.lblBandMember1.Text = "John Lennon- Guitar and Vocals"
+        Me.lblBandMember2.Text = "Ringo Starr – Drums and Vocals"
+        Me.lblBandMember3.Text = "Paul McCartney – Bass and Vocals"
+        Me.lblBandMember4.Text = "George Harrison – Lead Guitar and Vocals"
+        Me.lblBandMember5.Text = ""
+    End Sub
+
+    Private Sub displayRedHotChilliPeppersInfo(ByVal sender As Object, ByVal e As System.EventArgs) Handles radRedHotChilliPeppers.Click
+        Me.lblBandMember1.Text = "Anthony Kiedas – Vocals"
+        Me.lblBandMember2.Text = "Flea – Bass"
+        Me.lblBandMember3.Text = "Josh Klinghoffer – Lead Guitar"
+        Me.lblBandMember4.Text = "Chad Smith – Drums"
+        Me.lblBandMember5.Text = ""
+    End Sub
+
+    Private Sub displayFooFightersInfo(ByVal sender As Object, ByVal e As System.EventArgs) Handles radFooFighters.Click
+        Me.lblBandMember1.Text = "Dave Grohl – Guitar and Vocals"
+        Me.lblBandMember2.Text = "Taylor Hawkins – Drums"
+        Me.lblBandMember3.Text = "Pat Smear – Guitar"
+        Me.lblBandMember4.Text = "Nate Mendel – Bass"
+        Me.lblBandMember5.Text = "Chris Shiflett – Lead Guitar and Backing Vocals"
+    End Sub
+    Private Sub displayOneDirectionInfo(ByVal sender As Object, ByVal e As System.EventArgs) Handles radOneDirection.Click
+        Me.lblBandMember1.Text = "Harry Styles - Vocals"
+        Me.lblBandMember2.Text = "Zayn Malik - Vocals"
+        Me.lblBandMember3.Text = "Loius Tomlinson - Vocals"
+        Me.lblBandMember4.Text = "Niall Horan - Vocals"
+        Me.lblBandMember5.Text = "Liam Payne - Vocals"
+    End Sub
+
+    Private Sub displayNirvanaInfo(ByVal sender As Object, ByVal e As System.EventArgs) Handles radNirvana.Click
+        Me.lblBandMember1.Text = "Kurt Cobain – Guitar and Vocals"
+        Me.lblBandMember2.Text = "Dave Grohl – Drums"
+        Me.lblBandMember3.Text = "Krist Novoselic – Bass"
+        Me.lblBandMember4.Text = ""
+        Me.lblBandMember5.Text = ""
+    End Sub
+End Class
